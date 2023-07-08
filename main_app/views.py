44c0f554f50b0481ecfd57f8718378270ab7035d
mustafacev <mustafaorcuncevik@gmail.com>
2023-07-08 15:59:29 -0400

    defining views and routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+
+class Home(TemplateView):
+    template_name = "home.html"
+    
 
 
-class Home(View):
-    def get(self, request):
-        return HttpResponse("Real Estate Home")
-
-
-class About(View):
-    def get(self, request):
-        return HttpResponse("Real Estate About")
+class About(TemplateView):
+    template_name = "about.html"
+   
