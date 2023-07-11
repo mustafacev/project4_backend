19a0f2643b08f603a35e4cf91f9694d34ffcd68d
mustafacev <mustafaorcuncevik@gmail.com>
2023-07-10 22:09:58 -0400

    django admin created

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 import time
 # Create your models here.
+
+class House (models.Model):
+
+    title = models.CharField(max_length=100)
+    img = models.CharField(max_length=250)
+    address = models.TextField(max_length=200)
+    realtor = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode= models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    bedrooms = models.CharField(max_length=100)
+    bathroom = models.CharField(max_length=100)
+    sqft = models.CharField(max_length=100)
+    favorite_house = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
--- main_app/views.py
+++ main_app/views.py
@@ -12,7 +12,7 @@ class About(TemplateView):
     template_name = "about.html"
 
 class House:
-    def __init__(self, title, address, realtor ,city ,state ,zipcode ,price ,description ,image ,bedrooms,bathroom ,sqft,listdate):
+    def __init__(self, title, address, realtor ,city ,state ,zipcode ,price ,description ,image ,bedrooms,bathroom ,sqft):
         self.title = title
         self.address = address
         self.realtor = realtor
@@ -25,17 +25,17 @@ class House:
         self.bedrooms = bedrooms
         self.bathroom = bathroom
         self.sqft = sqft
-        self.listdate =listdate
+       
 
 house = [
-    House("Rocky Homes","40544 Briarhill Lane","kingston Real estate","Ohio","OH","044503","$134,000","Lakefront property","https://www.bellacollina.com/hubfs/Real%20Estate/Custom%20Built%20Homes.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
-    House("KESQL Homes","40544 Beverly Avenue","Nasq Real estate","Clington","NJ","044503","$341,000","Lakefront property","https://nelson-homes.com/media/images/Aries_prefab_homes_modular_homes.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
-    House("Lake Homes","40544 Bri st","Berlin Real estate","Ohio","OH","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Mensa_house_plan_prefab_homes_mo.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
-    House("Lakese Homes","40544 hill st","Atlas Real estate","Ohio","OH","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Preseus_prefab_homes_modular_hom.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
-    House("Nalsa Homes","40544 arhill st","Benja Real estate","Washington","NYC","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Sylvan_prefab_homes_modular_home.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
-    House("ASla Homes","40544 Brhill st","Kirlse Real estate","Boston","Ma","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Vermillion_prefab_homes_modular_.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
-    House("Derva Homes","40544 Briall st","Alss Real estate","Ohio","OH","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/McKinley_prefab_homes_modular_ho.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
-    House("Rcey Homes","40544 Rire st","kingston Real estate","Boston","MA","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Bayfield_prefab_homes_modular_ho.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
+    House("Rocky Homes","40544 Briarhill Lane","kingston Real estate","Ohio","OH","044503","$134,000","Lakefront property","https://www.bellacollina.com/hubfs/Real%20Estate/Custom%20Built%20Homes.jpg","2 bedrooms","1","70 sqft"),
+    House("KESQL Homes","40544 Beverly Avenue","Nasq Real estate","Clington","NJ","044503","$341,000","Lakefront property","https://nelson-homes.com/media/images/Aries_prefab_homes_modular_homes.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft"),
+    House("Lake Homes","40544 Bri st","Berlin Real estate","Ohio","OH","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Mensa_house_plan_prefab_homes_mo.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft"),
+    House("Lakese Homes","40544 hill st","Atlas Real estate","Ohio","OH","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Preseus_prefab_homes_modular_hom.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft"),
+    House("Nalsa Homes","40544 arhill st","Benja Real estate","Washington","NYC","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Sylvan_prefab_homes_modular_home.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft"),
+    House("ASla Homes","40544 Brhill st","Kirlse Real estate","Boston","Ma","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Vermillion_prefab_homes_modular_.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft"),
+    House("Derva Homes","40544 Briall st","Alss Real estate","Ohio","OH","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/McKinley_prefab_homes_modular_ho.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft"),
+    House("Rcey Homes","40544 Rire st","kingston Real estate","Boston","MA","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Bayfield_prefab_homes_modular_ho.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft"),
 ]
 
 class HouseList(TemplateView):
