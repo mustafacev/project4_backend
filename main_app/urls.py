4f6e890550e79809fbe62b27e4421d9a64fd4179
mustafacev <mustafaorcuncevik@gmail.com>
2023-07-10 21:49:44 -0400

    house list added

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
-    path('about/', views.About.as_view(), name="about"), 
+    path('about/', views.About.as_view(), name="about"),
+    path('house/', views.HouseList.as_view(), name="house_list") 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -10,4 +10,38 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = "about.html"
-   
+
+class House:
+    def __init__(self, title, address, realtor ,city ,state ,zipcode ,price ,description ,image ,bedrooms,bathroom ,sqft,listdate):
+        self.title = title
+        self.address = address
+        self.realtor = realtor
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.price = price
+        self.description = description
+        self.image = image
+        self.bedrooms = bedrooms
+        self.bathroom = bathroom
+        self.sqft = sqft
+        self.listdate =listdate
+
+house = [
+    House("Rocky Homes","40544 Briarhill Lane","kingston Real estate","Ohio","OH","044503","$134,000","Lakefront property","https://www.bellacollina.com/hubfs/Real%20Estate/Custom%20Built%20Homes.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
+    House("KESQL Homes","40544 Beverly Avenue","Nasq Real estate","Clington","NJ","044503","$341,000","Lakefront property","https://nelson-homes.com/media/images/Aries_prefab_homes_modular_homes.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
+    House("Lake Homes","40544 Bri st","Berlin Real estate","Ohio","OH","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Mensa_house_plan_prefab_homes_mo.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
+    House("Lakese Homes","40544 hill st","Atlas Real estate","Ohio","OH","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Preseus_prefab_homes_modular_hom.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
+    House("Nalsa Homes","40544 arhill st","Benja Real estate","Washington","NYC","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Sylvan_prefab_homes_modular_home.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
+    House("ASla Homes","40544 Brhill st","Kirlse Real estate","Boston","Ma","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Vermillion_prefab_homes_modular_.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
+    House("Derva Homes","40544 Briall st","Alss Real estate","Ohio","OH","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/McKinley_prefab_homes_modular_ho.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
+    House("Rcey Homes","40544 Rire st","kingston Real estate","Boston","MA","044503","$134,000","Lakefront property","https://nelson-homes.com/media/images/Bayfield_prefab_homes_modular_ho.2e16d0ba.fill-1920x1080.jpg","2 bedrooms","1","70 sqft","23/06/2023"),
+]
+
+class HouseList(TemplateView):
+    template_name = "house_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["house"] = house
+        return context
